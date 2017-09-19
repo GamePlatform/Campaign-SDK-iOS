0f20b67335b6a336dbd236da4d7a61471aedcca7
hyeongyun0916 <mhg5303@gmail.com>
2017-09-19 15:04:37 +0900

    Post Analytics array with JSON string.

--- CampaignPlatform/CampaignPlatform/Utils/APIManager/APIManager.m
+++ CampaignPlatform/CampaignPlatform/Utils/APIManager/APIManager.m
@@ -186,8 +186,10 @@ constructingBodyWithBlock:^(id<AFMultipartFormData> formData){if(formDataCallbac
 
 - (void)postAnalytics:(NSString*)inform analytics:(NSArray *)analytics success:(NetworkSucBlock)success {
     NSMutableDictionary *param = [NSMutableDictionary new];
-    [param setObject:analytics forKey:@"analytics"];
+    [param setObject:[[NSString alloc] initWithData:
+                      [NSJSONSerialization dataWithJSONObject:analytics options:NSJSONWritingPrettyPrinted error:nil] encoding:NSUTF8StringEncoding] forKey:@"analytics"];
     [param setObject:_deviceID forKey:@"deviceID"];
+    
     [self post:[NSString stringWithFormat:@"api/apps/%@/analytics", _appID] parameters:param inform:inform formData:nil progress:nil success:success failFromServer:nil completion:nil];
 }
 
